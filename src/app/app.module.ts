5bb1ed0f43422f4930e351fb4079a2f0f33ddcdf
Amadou Sall <ahasall.dev@gmail.com>
2020-08-26 15:51:04 +0200

    add reproduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LocalizeParser, LocalizeRouterModule, LocalizeRouterSettings, ManualParserLoader } from '@gilsdav/ngx-translate-router';
+import { Location } from '@angular/common';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { MatButtonModule } from '@angular/material/button';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+export function createManualParserLoader(translate: TranslateService, location: Location, settings: LocalizeRouterSettings) {
+  return new ManualParserLoader(translate, location, settings, ['fr', 'en']);
+}
+
+export function defaultLangFunction(languages: string[], cachedLang?: string, browserLang?: string) {
+  return 'fr';
+}
+
+const routes: Routes = [];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    MatButtonModule,
+    RouterModule.forRoot(routes),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
+    LocalizeRouterModule.forRoot(routes, {
+      parser: {
+        provide: LocalizeParser,
+        useFactory: createManualParserLoader,
+        deps: [TranslateService, Location, LocalizeRouterSettings],
+      },
+      defaultLangFunction: defaultLangFunction,
+    }),
   ],
+  declarations: [AppComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
